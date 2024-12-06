73152912a52e5d1c1b3124032137c1af45f80cae
Nourelhoudaassiddeki <159466193+Nourelhoudaassiddeki@users.noreply.github.com>
2024-12-06 19:43:31 +0100

    Add all project files

---
+++ src/supprimeterrain.java
@@ -0,0 +1,32 @@
+
+
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.PreparedStatement;
+    import java.util.Scanner;
+    
+    
+    public class supprimeterrain {
+        public static void main(String[] args) {
+            // Database connection details
+            String url = "jdbc:postgresql://localhost:5432/demo";
+            String username = "postgres";
+            String password = "thebigboss"; 
+    Scanner scann= new Scanner(System.in);
+    System.out.println("Enter your id");
+    int id=scann.nextInt();
+            String query = "delete FROM terrains where id_terrain=?";
+         
+            try {
+                Connection conn = DriverManager.getConnection(url, username, password);
+              PreparedStatement st=conn.prepareStatement(query);
+                st.setInt(1, id);
+                st.executeUpdate();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            scann.close();
+        }
+    }
+
+
---
+++ src/updatevent.java
@@ -0,0 +1,24 @@
+
+
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.PreparedStatement;
+    public class updatevent {
+        public static void main(String[] args) {
+            // Database connection details
+            String url = "jdbc:postgresql://localhost:5432/demo";
+            String username = "postgres";
+            String password = "thebigboss"; 
+ 
+            String query = "UPDATE events SET nom_event='reunion',description='exams schedule' WHERE nom_event='weeding'";
+         
+            try {
+                Connection conn = DriverManager.getConnection(url, username, password);
+              PreparedStatement st=conn.prepareStatement(query);
+                st.executeUpdate();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
